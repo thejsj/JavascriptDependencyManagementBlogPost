e6c7f69d117c5c8e95cc9b5ff4c4c722d1005020
thejsj <jorge.silva@thejsj.com>
2014-07-10 23:53:57 -0400

    Switching to canvas. Adding some styles

--- app/main.js
+++ app/main.js
@@ -1,26 +1,70 @@
 var MovingCircle = require('./moving-circle');
 var jQuery       = require('jquery');
+var _            = require('underscore');
 require('../bower_components/gsap/src/uncompressed/TweenMax.js');
 
 var MovingCircleHandler; 
 
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+window.paused = false;
 (function ($){
 
     MovingCircleHandler = function () {
 
         this.circles = [];
         this.$el = $('.circle-container');
-
+        
         this.init = function () {
-
             var i;
-            for (i = 0; i < 10; i += 1) {
+
+            this.canvas = this.$el.get(0);
+            this.canvas.width = this.$el.width();
+            this.canvas.height = this.$el.height();
+            this.context = this.canvas.getContext('2d');
+
+            for (i = 0; i < 50; i += 1) {
                 this.circles.push(new MovingCircle(i, {
-                    'width' : this.$el.width(),
-                    'height' : this.$el.height(),
-                }, this.$el));
+                    'width' : this.canvas.width,
+                    'height' : this.canvas.height,
+                }, this.context, false));
             }
+
+            this.$el.click(_.bind(this.append, this));
+
+            requestAnimationFrame(_.bind(this.update, this));
+            $(window).resize(_.bind(this.reset, this));
         };
+
+        this.update = function () {
+            if (!window.paused) {
+                this.context.clearRect ( 0, 0, this.canvas.width, this.canvas.height);
+                for (i = 0; i < this.circles.length; i += 1) {
+                    this.circles[i].update();
+                }
+            }
+            requestAnimationFrame(_.bind(this.update, this));
+        }; 
+
+        this.append = function (event) {
+            console.log(event);
+            this.circles.push(new MovingCircle(i, {
+                'width' : this.canvas.width,
+                'height' : this.canvas.height,
+            }, this.context, {
+                'x' : event.clientX,
+                'y' : event.clientY
+            }));
+        };
+
+        this.reset = function () {
+            for (i = 0; i < this.circles.length; i += 1) {
+                this.circles[i].delete();
+            }
+            delete this.circles;
+            this.init();
+        }
 
         this.init();
         return this;
--- app/moving-circle.js
+++ app/moving-circle.js
@@ -1,28 +1,63 @@
 var jQuery       = require('jquery');
+var _            = require('underscore');
 
 var MovingCircle; 
 
 (function ($){
 
-    MovingCircle = function (index, width_and_height, $container) {
+    MovingCircle = function (index, width_and_height, context, position) {
 
         this.init = function () {
             this.id = "circle-" + index;
             this.className = 'single-circle';
-            this.x = Math.random() * width_and_height.width;
-            this.y = Math.random() * width_and_height.height;
-            this.$container = $container; 
+            this.speed = 5;
+            this.context = context;
 
-            $container
-                .append('<div id="' + this.id + '" class="' + this.className + '">');
+            if(position) {
+                this.x = position.x;
+                this.y = position.y;
+            } else {
+                this.x = _.random(1, width_and_height.width);
+                this.y = _.random(1, width_and_height.height);
+            }
+            this.up   = ((Math.random() > 0.5) ? true : false);
+            this.left = ((Math.random() > 0.5) ? true : false);
 
-            this.$el = $('#' + this.id);
+            this.size = 5;
 
-            this.$el
-                .css('background', 'red')
-                .css('top', this.y)
-                .css('left', this.x);
+            this.color = {
+                'r' : _.random(50, 255),
+                'g' : _.random(50, 255),
+                'b' : _.random(50, 255)
+            };
+            this.color_a = 'rgb(' + this.color.r + ',' + this.color.g + ',' + this.color.b + ')';
+            this.color_b = 'rgb(' + (this.color.r - 50) + ',' + (this.color.g - 50) + ',' + (this.color.b - 50) + ')';
+            this.color_s = 'linear-gradient(' + this.color_b + ',' + this.color_a + ')';  
+        };
 
+        this.delete = function () {
+            this.$el.remove();
+        };
+
+        this.update = function () {
+            this.x = ((this.left) ? this.x += this.speed : this.x -= this.speed);
+            this.y = ((this.up) ? this.y += this.speed : this.y -= this.speed);
+
+            if(this.x <= 0 || this.x >= width_and_height.width) {
+                this.left = !this.left;
+            }
+            if(this.y <= 0 || this.y >= width_and_height.height) {
+                this.up = !this.up;
+            }
+            this.context.beginPath();
+            this.context.arc(this.x, this.y, this.size, 0, 2 * Math.PI, false);
+
+            this.gradient = this.context.createLinearGradient(this.x,this.y,this.x + this.size, this.y + this.size);
+            this.gradient.addColorStop(1,this.color_a);
+            this.gradient.addColorStop(0,this.color_b);
+
+            this.context.fillStyle = this.gradient;
+            this.context.fill();
         };
 
         this.init();
